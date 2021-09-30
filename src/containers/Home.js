213e9c4cb26baa0b7c522209a324e55737b3f7c4
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-09-30 16:14:16 -0500

    Fixed behavior of carousel buttons

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -15,9 +15,9 @@ const Home = (props) => {
     useEffect(() => {
         if (active) {
             const timer = setTimeout( () => {
+                document.getElementById(`tom${(index + 1) % 3}`).style.backgroundColor = 'blueviolet';
                 document.getElementById(`tom${index}`).style.backgroundColor = 'white';
                 setIndex(prev => (prev + 1) % 3);
-                document.getElementById(`tom${index}`).style.backgroundColor = 'blueviolet';
             }, 5000);
             return () => {
                 clearTimeout(timer);
@@ -27,6 +27,12 @@ const Home = (props) => {
 
     const handleClick = (event) => {
         setActive(false);
+        const imgs = document.getElementsByClassName('img-button');
+        for (let img of imgs) {
+            img.style.backgroundColor = 'white';
+        }
+        event.target.style.backgroundColor = 'blueviolet';
+
         setIndex(Number(event.target.id[3]));
     } 
 
