9a66af629bd0eaed4adba66367ab1ae018365c1d
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-09-30 18:13:58 -0500

    Added CSS responsive behavior

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
+import '../App.css';
 
 const Nav = () => {
+    const handleHamburgerClick = ({ target }) => {
+        document.getElementById('hamburger').style.visibility = 'hidden';
+        document.getElementById('close').style.visibility = 'visible';
+        document.getElementById('nav-box').style.visibility = 'visible';
+        document.querySelector('nav').style.height = '100%';
+    }
+
+    const handleCloseClick = ({ target }) => {
+        document.getElementById('close').style.visibility = 'hidden';
+        document.getElementById('hamburger').style.visibility = 'visible';
+        document.getElementById('nav-box').style.visibility = 'hidden';
+        document.querySelector('nav').style.height = '50px';
+    }
+    
     return (
         <nav>
-            <div className="nav-items">
-                <Link to="/#banner">Home</Link>
+            <div id="nav-box">
+                <div className="nav-items" onClick={handleCloseClick}>
+                    <Link to="/#banner">Home</Link>
+                </div>
+                <div className="nav-items" onClick={handleCloseClick}>
+                    <Link to="/#bio">Bio</Link>
+                </div>
+                <div className="nav-items" onClick={handleCloseClick}>
+                    <Link to="/#audio">Recordings</Link>
+                </div>
+                <div className="nav-items" onClick={handleCloseClick}>
+                    <Link to="/#performances">Performances</Link>
+                </div>
+                <div className="nav-items" onClick={handleCloseClick}>
+                    <Link to="/#quote">Quote</Link>
+                </div>
+                <div className="nav-items" onClick={handleCloseClick}>
+                    <Link to="/#contact">Contact Me</Link>
+                </div>
             </div>
-            <div className="nav-items">
-                <Link to="/#bio">Bio</Link>
+            <div id="hamburger" onClick={handleHamburgerClick} >
+                <i class="fas fa-bars"></i>
             </div>
-            <div className="nav-items">
-                <Link to="/#audio">Recordings</Link>
-            </div>
-            <div className="nav-items">
-                <Link to="/#performances">Performances</Link>
-            </div>
-            <div className="nav-items">
-                <Link to="/#quote">Quote</Link>
-            </div>
-            <div className="nav-items">
-                <Link to="/#contact">Contact Me</Link>
+            <div id="close" onClick={handleCloseClick}>
+                <i class="fas fa-times"></i>
             </div>
         </nav>
     )
