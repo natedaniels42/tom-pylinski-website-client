e828e325632f556df29d15a3bdae2fa5cb87df7e
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-10-26 16:29:05 -0500

    Added Links component

---
+++ src/components/Links.js
@@ -0,0 +1,36 @@
+const Links = () => {
+    return (
+        <div>
+            <h2>Helpful Links:</h2>
+            <ul>
+                <li>
+                    <a href="https://www.edwards-instruments.com/" target="_blank">
+                        Edwards Instruments | Custom Trombones and Trumpets
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.hickeys.com/" target="_blank">
+                        Hickey's Music Center
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.woodsysmusic.com/" target="_blank">
+                        Woodsy's Music
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.pellegrinomusic.com/" target="_blank">
+                        Pellegrino Music & Lesson Center
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.mountunion.edu/dept-of-performing-arts" target="_blank">
+                        University of Mount Union | Department of Performing Arts
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Links;
--- src/containers/Resources.js
+++ src/containers/Resources.js
@@ -1,7 +1,10 @@
+import Links from "../components/Links";
+
 const Resources = () => {
     return (
         <div id="resource-container">
             <h1>Student Resources</h1>
+            <Links />
         </div>
     )
 }
