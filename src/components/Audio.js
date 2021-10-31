ae7066de2d176e169c5a07d4952ede26a0ad1b64
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-10-30 20:04:09 -0500

    Updated the audio files from m4a to mp3

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -6,15 +6,15 @@ const Audio = () => {
             <h2>RECORDINGS</h2>
             <div className="audio-container">
                 <p>Schumann: 3 Romances</p>
-                <audio controls src="/schumann.m4a"></audio>
+                <audio controls src="/schumann.mp3"></audio>
             </div>
             <div className="audio-container">
                 <p>Ravel: Bolero</p>
-                <audio controls src="/tom-bolero.m4a"></audio>
+                <audio controls src="/bolero.mp3"></audio>
             </div>
             <div className="audio-container">
                 <p>Bach: Cello Suite No. 1, Gigue</p>
-                <audio controls src="/tom-bach.m4a"></audio>
+                <audio controls src="/bach.mp3"></audio>
             </div>
         </div>
     )
