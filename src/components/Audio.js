cf1dcc68daed7663a41336d745ca35db03c50f47
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-10-31 15:48:58 -0500

    Updated resume

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -5,15 +5,15 @@ const Audio = () => {
         <div id="audio">
             <h2>RECORDINGS</h2>
             <div className="audio-container">
-                <p>Schumann: 3 Romances</p>
+                <p>Robert Schumann: 3 Romances, I. Nicht Schnell</p>
                 <audio controls src="/schumann.mp3"></audio>
             </div>
             <div className="audio-container">
-                <p>Ravel: Bolero</p>
+                <p>Maurice Ravel: Bolero</p>
                 <audio controls src="/bolero.mp3"></audio>
             </div>
             <div className="audio-container">
-                <p>Bach: Cello Suite No. 1, Gigue</p>
+                <p>J.S. Bach: Cello Suite No. 1, Gigue</p>
                 <audio controls src="/bach.mp3"></audio>
             </div>
         </div>
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -42,7 +42,7 @@ const Bio = () => {
                 of Local 24 of the American Federation of Musicians. He makes 
                 his home in Munroe Falls with his wife Janice, who is a music 
                 therapist, and their son Matthew.</p><br /><br />
-            <p>Click <a href="tom-pylinski-music-resume.pdf" target="_blank">here</a> to view Tom's resume.</p>
+            <p>Click <a href="tom-resume.pdf" target="_blank">here</a> to view Tom's resume.</p>
         </div>
     )
 }
